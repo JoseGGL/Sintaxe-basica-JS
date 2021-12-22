61a3ea40f8b532030dc9f4c8d9653ade19eb5866
JoseGGL <aurivanx1@gmail.com>
2021-12-22 11:11:03 -0300

    script com laços condicionais

---
+++ condicional/script.js
@@ -0,0 +1,44 @@
+var A = 0;
+var B = 2;
+var comparacao;
+var array = ['a1', 'a2', 'a3'];
+var object = { prop1: 01, };
+
+A > 0 && B > 0 ? console.log('As variaveis são aceitáveis') :
+    console.log ('Alguma variavel possui valor 0 ou menor.');
+ if (B > 0) {
+    console.log('B sofreu incremento');
+    } else {
+        console.log('Nada mudou');
+    }
+switch (comparacao) {
+    case comparacao = A > B:
+        console.log('A é maior que B');
+        break;
+    case placar = B > A:
+        console.log('B é maior que A');
+        break;
+        default:
+            console.log('As variaveis sao iguais');
+}
+for (let teste = 0; teste < array.length; teste++) {
+    console.log(teste);
+}
+
+for (let i in array){
+    console.log(i);
+}
+
+for (b in object){
+    console.log(b);
+}
+
+for (a of array){
+    console.log(a);
+}
+
+var a1 = 0
+while (a1 < 10){
+    a++
+    console.log (a1);
+}
